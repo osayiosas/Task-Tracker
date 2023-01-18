22b2027c2f85e676eee922ad313df0b25051403f
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-18 16:26:38 +0100

    Tracking project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
-import Header from './components/Header';
-import Task from './components/Task';
-import { useState } from "react"
+import { useState } from "react";
+import Header from "./components/Header";
+import Task from "./components/Task";
+import AddTask from "./components/AddTask";
 
+const App = () => {
+  const [task, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Working from Home",
+      day: "feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "food Shopping",
+      day: "feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
 
+  //add Task
 
-const App = () =>
-{
-  
-   const [task, setTask] = useState(
-      [  {
-            id: 1, 
-            text: 'Doctors Appointment',
-            day: 'feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2, 
-            text: 'Working from Home',
-            day: 'feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 3, 
-            text: 'food Shopping',
-            day: 'feb 5th at 2:30pm',
-            reminder: false,
-        },
-     ])
-  
-  //Delet Task 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
 
-  const deleteTask = (id) =>
-  {
-    setTask(task.filter((task) => task.id !== id))
-  }
+    const newTask = { id, ...task };
 
-  //toggle Reminder 
+    setTask([...task, newTask]);
+  };
 
-  const toggleReminder = (id) =>
-  {
-    console.log('id')
-  }
+  //Delet Task
 
-  
+  const deleteTask = (id) => {
+    setTask(task.filter((task) => task.id !== id));
+  };
+
+  //toggle Reminder
+
+  const toggleReminder = (id) => {
+    setTask(
+      task.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-
-      <Header title='Task Tracker' />
-
-      {task.length > 0 ? <Task task={task} onDelete=
-        
-      {deleteTask} onToggle={toggleReminder} /> : 'no Tasks to show'}
+      <Header title="Task Tracker" />
+      <AddTask onAdd={addTask} />
+      {task.length > 0 ? (
+        <Task task={task} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "no Tasks to show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 const Tasks = ({ task,  onDelete, onToggle }) => 
 {
   return (
-      <div className='tasks' onDoubleClick={() => onToggle(task.id)}>
+      <div className={`tasks ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
           
             <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} /> </h3>
           <p>{task.day}</p>
